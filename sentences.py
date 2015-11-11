a808c318d059db43bd63f7cd93e79852853ad1dd
Johannes Jasper <johannes.jasper@student.hpi.de>
2015-11-11 16:16:40 +0100

    start on SVM

--- sentences.py
+++ sentences.py
@@ -1,52 +1,56 @@
 import xml.etree.ElementTree as ET
 import os
-import json
 import collections
 import sys
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import numpy as np
-import matplotlib.pyplot as plt
 
-
+from sklearn.feature_extraction.text import CountVectorizer
 filepath = sys.argv[1]
-files = []
-sentences = []
+
+vectorizer = CountVectorizer(min_df=1, stop_words=loadStopWords())
+X = vectorizer.fit_transform(sentences(filepath))
+print(X)
+
+
 
 pair_types = ['effect', 'mechanism', 'advise', 'int']
 entity_types = ['drug', 'drug_n', 'group', 'brand']
 
 def find_sentence(filename):
+    sentences = []
+
     tree = ET.parse(filename)
     root = tree.getroot()
 
     for document in root.iter('document'):
         for sentence in document.findall('sentence'):
-            tokens = []
+            # tokens = []
             for pair in sentence.findall('pair'):
                 ddi = pair.get('ddi')
                 if (ddi == "true") and (pair.get('type') == sys.argv[2]):
-                    text = sentence.get('text').split(' ')
-                    for w in text:
-                        w = w.lower()
-                        w = w.translate(None, '.,%;:*!@#$&*()+=')
-                        tokens.append(w)
-            sentences.append(tokens)
+                    text = sentence.get('text')
+                    # text = sentence.get('text').split(' ')
+                    # for w in text:
+                    #     w = w.lower()
+                    #     w = w.translate(None, '.,%;:*!@#$&*()+=')
+                    #     tokens.append(w)
+                    sentences.append(text)
+    return sentences
 
 
-def tokenLists(filepath):
+def sentences(filepath):
+    sentences = []
     for filename in os.listdir(filepath):
        if (".xml" in filename):
-           files.append(filepath+filename)
-           find_sentence(filename)
+           sentences.extend(find_sentence(filename))
 
     if sys.argv[2] == 'stop':
         stops = loadStopWords()
+        for sentence in sentences:
+            for word in sentence:
+                if word in stops:
+                    sentence.remove(word)
 
-
-        for stop in stops:
-            try:
-                del counted_words[stop]
-            except(KeyError): pass
+    return sentences
 
 def loadStopWords():
     stops = []
