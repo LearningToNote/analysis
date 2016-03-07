6fc059fc0e199aefa32bbfb09b2eaec99269a020
Mariya Perchyk <mariya.perchyk@studen.hpi.de>
2016-03-07 18:31:31 +0100

    aggregation over multiple learning curves

--- current/local.r
+++ current/local.r
@@ -5,11 +5,14 @@ ltn.downsample <- function(data) {
 	#### down sampling
 	true_pairs <- data[data$DDI != -1,]
 	false_pairs <- data[data$DDI == -1,]
-	false_downsampled_index <- sample(1:nrow(false_pairs), nrow(true_pairs) * 1.5)
-	false_downsampled <- false_pairs[false_downsampled_index,]
-
-	data <- rbind(true_pairs, false_downsampled)
-	return(data)
+	if (nrow(true_pairs) > 0) {
+		false_downsampled_index <- sample(1:nrow(false_pairs), min(nrow(false_pairs), nrow(true_pairs)*1.5) )
+		false_downsampled <- false_pairs[false_downsampled_index,]
+		data <- rbind(true_pairs, false_downsampled)
+		return(data)
+	} else {
+		return(false_pairs)
+	}
 }
 
 ltn.splitCopurs <- function(path) {
@@ -36,7 +39,6 @@ ltn.sampleN <- function(data, n, seed) {
 
 	train_rows <- which(data$DOC_ID %in% train_docs)
 	test_rows <- which(data$DOC_ID %in% test_docs)
-
 	train_data <- ltn.downsample(data[train_rows,-1])
 	test_data <- data[test_rows,-1]
 
@@ -44,6 +46,16 @@ ltn.sampleN <- function(data, n, seed) {
 }
 
 ltn.train_binary <- function(data) {
+	ddi <- data$DDI
+
+	if (length(unique(ddi)) == 1) {
+		return(NULL)
+	}
+	#### binarize target values
+
+	ddi[ddi != -1] <- 1
+	ddi[ddi == -1] <- 0
+
 	#### feature extraction
 
 	before_dtm <- create_matrix(
@@ -87,12 +99,6 @@ ltn.train_binary <- function(data) {
 			data$E1_TYPE, data$E2_TYPE,
 			data$CHAR_DIST, data$WORD_DIST)
 
-	ddi <- data$DDI
-
-	#### binarize target values
-
-	ddi[ddi != -1] <- 1
-	ddi[ddi == -1] <- 0
 	container <- create_container(features,ddi,trainSize=1:nrow(data),virgin=FALSE)
 
 	svm.model.binary <- train_models(container, algorithms=c("SVM"), method = "C-classification")
@@ -111,7 +117,11 @@ ltn.train_binary <- function(data) {
 }
 
 ltn.train_multiclass <- function(data,dictionaries) {
+
 	data <- data[data$DDI != -1,]
+	if (length(unique(data$DDI)) <= 1) {
+		return(NULL)
+	}
 
 	o_before_dtm=dictionaries$o_before_dtm
 	o_between_dtm=dictionaries$o_between_dtm
@@ -176,6 +186,9 @@ ltn.train_multiclass <- function(data,dictionaries) {
 }
 
 ltn.predict <- function(svm.model.binary, svm.model.classes, data, dictionaries) {
+	if (is.null(svm.model.binary) || is.null(svm.model.classes)) {
+		return(NULL)
+	}
 	o_before_dtm=dictionaries$o_before_dtm
 	o_between_dtm=dictionaries$o_between_dtm
 	o_after_dtm=dictionaries$o_after_dtm
@@ -279,29 +292,25 @@ ltn.recall <- function(data, type) {
 	actual = data[,1]
 	tp = length(which(predicted == type & actual == type))
 	fn = length(which(predicted != type & actual == type))
-	print(data)
 	if (tp+fn > 0) {
 		return(tp/(tp+fn))
 		} else {
-			return(0)
+			return(0.0)
 		}
 }
 
 ltn.precision.collection <- function(data) {
 	# types cantained in the actual test data
-	types = sort(union(unique(data[,1]), unique(data[,2])))
+	types = sort(unique(c(data[,1],data[,2])))
 	result = matrix(ncol=4, nrow=0)
 	for (type in types) {
 		p = ltn.precision(data, type)
 		r = ltn.recall(data, type)
-		print(p)
-		print(r)
 		if (p+r > 0) {
 			f = 2*p*r/(p+r)
 			} else {
-				f = 0
+				f = 0/1
 			}
-
 		result <- rbind (result, c(type,round(p,2),round(r,2),round(f,2)))
 	}
 	result <- as.data.frame(result)
@@ -314,11 +323,11 @@ path = '/Users/mariyaperchyk/Documents/python_hana/analysis/rData/data.csv'
 
 
 ################MULTIPLE ITERATIONS
-ltn.iterations <- function(path) {
+ltn.iterations <- function(path,iterations) {
 	result = matrix(ncol=4, nrow=0)
 	colnames(result) <- c('TYPE', 'PRECISION', 'RECALL', 'F_MEASURE')
 
-	for(i in 1:10){
+	for(i in 1:iterations){
 		print ("------------------------")
 		print (i)
 		splitData <- ltn.splitCopurs(path)
@@ -335,11 +344,9 @@ ltn.iterations <- function(path) {
 
 		print('predicting')
 		predicted <- ltn.predict(svm.model.binary, svm.model.classes, test_data[,-1], dictionaries)
-		predicted <- predicted[,1]
+		predicted <- as.numeric(levels(predicted[,1]))[predicted[,1]]
 		actual <- test_data[,1]
-
-		a <- ltn.precision.collection(cbind(actual, as.data.frame(predicted)))
-		a
+		a <- ltn.precision.collection(cbind(actual,predicted))
 		result = rbind(result,a)
 	}
 
@@ -347,15 +354,14 @@ ltn.iterations <- function(path) {
 }
 
 ############LEARNING CURVE
-ltn.learn <- function(path) {
-	data <- read.csv(path)
-	result = matrix(ncol=4, nrow=0)
-	colnames(result) <- c('TYPE', 'PRECISION', 'RECALL', 'F_MEASURE')
+ltn.learn <- function(data,number_of_iterations, seed) {
+	result = matrix(ncol=5, nrow=0)
+	colnames(result) <- c('TYPE', 'PRECISION', 'RECALL', 'F_MEASURE','i')
 
-	for(i in 1:10){
+	for(i in 1:number_of_iterations){
 
 		print ("------------------------")
-		splitData <- ltn.sampleN(data, i, 42)
+		splitData <- ltn.sampleN(data, i, seed)
 		train_data = splitData$train_data
 		test_data = splitData$test_data
 
@@ -369,14 +375,36 @@ ltn.learn <- function(path) {
 
 		print('predicting')
 		predicted <- ltn.predict(svm.model.binary, svm.model.classes, test_data[,-1], dictionaries)
+		if (is.null(predicted)) {
+			result = rbind(result,c(rep(0,4),i))
+			next
+		}
 		predicted <- predicted[,1]
+		if (class(predicted) !='numeric') {
+			predicted <- as.numeric(levels(predicted))[predicted]
+		}
 		actual <- test_data[,1]
 
-		a <- ltn.precision.collection(cbind(actual, as.data.frame(predicted)))
-		print(a)
+		a <- ltn.precision.collection(cbind(actual, predicted))
+		a <- cbind(a,i)
 		result = rbind(result,a)
 	}
+	print(result)
 	return(result)
+}
+
+ltn.aggregate_learn <- function(path, iterations_per_cycle, cycles,seed) {
+	# cycles = how often should we execute the learning curve to aggregate
+	# iterations_per_cycle = how many docs at max we use to learn
+	data <- read.csv(path)
+	result = matrix(ncol=5, nrow=0)
+	colnames(result) <- c('TYPE', 'PRECISION', 'RECALL', 'F_MEASURE','i')
+
+	for(i in 1:cycles){
+		a <- ltn.learn(data,iterations_per_cycle,i+seed)
+		result <-rbind(result, a)
+	}
+	return(aggregate(. ~ TYPE+i, data= result, FUN="mean"))
 }
 
 plot_f1 <- function(a) {
@@ -387,5 +415,5 @@ plot_f1 <- function(a) {
 	lines(1:nrow(a[a$TYPE==140,]),a[a$TYPE==140,4],col="yellow",xlim=c(1,nrow(a[a$TYPE==-1,])),ylim=c(0,1))
 }
 
-#a <- ltn.learn(path)
+#a <- ltn.learn(path,2,102)
 #plot_f1(a)
